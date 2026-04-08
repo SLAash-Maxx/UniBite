ff001d5fbc53e04b4a1fae7d003babe87b35ab8b
Anuhas Diwyanjana <anuhasdiwyanjanapubg@gmail.com>
2026-04-09 01:22:56 +0530

    Add files via upload

---
+++ lib/core/constants/app_assets.dart
@@ -0,0 +1,43 @@
+class AppAssets {
+  AppAssets._();
+
+  // Base paths
+  static const String _images = 'assets/images/';
+  static const String _icons  = 'assets/icons/';
+
+  // Logos & branding
+  static const String logo         = '${_images}logo.png';
+  static const String logoWhite    = '${_images}logo.png';
+  static const String nsbmLogo      = '${_images}nsbm_logo.png';
+  static const String splashBg     = '${_images}splash_bg.png';
+
+  // Onboarding / empty states
+  static const String emptyCart    = '${_images}empty_cart.png';
+  static const String emptyOrders  = '${_images}empty_orders.png';
+  static const String notFound     = '${_images}not_found.png';
+  static const String errorState   = '${_images}error_state.png';
+  static const String noInternet   = '${_images}no_internet.png';
+
+  // Placeholder / fallback
+  static const String foodPlaceholder   = '${_images}food_placeholder.png';
+  static const String avatarPlaceholder = '${_images}avatar_placeholder.png';
+  static const String bannerPlaceholder = '${_images}banner_placeholder.png';
+
+  // Bottom nav icons (SVG)
+  static const String iconHome     = '${_icons}home.svg';
+  static const String iconShop     = '${_icons}shop.svg';
+  static const String iconCart     = '${_icons}cart.svg';
+  static const String iconOrders   = '${_icons}orders.svg';
+  static const String iconProfile  = '${_icons}profile.svg';
+
+  // Misc icons (SVG)
+  static const String iconSearch   = '${_icons}search.svg';
+  static const String iconFilter   = '${_icons}filter.svg';
+  static const String iconStar     = '${_icons}star.svg';
+  static const String iconBell     = '${_icons}bell.svg';
+  static const String iconBack     = '${_icons}back.svg';
+  static const String iconEdit     = '${_icons}edit.svg';
+  static const String iconDelete   = '${_icons}delete.svg';
+  static const String iconLocation = '${_icons}location.svg';
+  static const String iconClock    = '${_icons}clock.svg';
+}
