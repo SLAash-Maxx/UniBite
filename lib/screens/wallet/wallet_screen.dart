4cb90181e5dc5791a4fa8b7824d9205e80b80bfb
Diluminda_Peiris <thelgedilu@gmail.com>
2026-04-09 02:09:33 +0530

    Remove WalletScreen widget definition
    Removed WalletScreen widget definition to streamline code.

--- lib/screens/wallet/wallet_screen.dart
+++ lib/screens/wallet/wallet_screen.dart
@@ -7,11 +7,7 @@ import '../../core/utils/formatters.dart';
 import '../../models/wallet_model.dart';
 import '../../providers/wallet_provider.dart';
 
-class WalletScreen extends StatefulWidget {
-  const WalletScreen({super.key});
-  @override
-  State<WalletScreen> createState() => _WalletScreenState();
-}
+
 
 class _WalletScreenState extends State<WalletScreen> {
   @override
