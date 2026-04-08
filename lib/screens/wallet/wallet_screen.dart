1f1482302626992297ae95cc175668ec7e96da72
Diluminda_Peiris <thelgedilu@gmail.com>
2026-04-09 02:13:34 +0530

    Add WalletScreen widget to wallet_screen.dart

--- lib/screens/wallet/wallet_screen.dart
+++ lib/screens/wallet/wallet_screen.dart
@@ -7,7 +7,11 @@ import '../../core/utils/formatters.dart';
 import '../../models/wallet_model.dart';
 import '../../providers/wallet_provider.dart';
 
-
+class WalletScreen extends StatefulWidget {
+  const WalletScreen({super.key});
+  @override
+  State<WalletScreen> createState() => _WalletScreenState();
+}
 
 class _WalletScreenState extends State<WalletScreen> {
   @override
